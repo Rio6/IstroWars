a789020bcdfb4b91761963440e9cb5cfe045f1d8
Rio Liu <rio.liu@r26.me>
2021-11-24 22:55:15 -0500

    kick factions with low influence in tick

--- src/tick/index.ts
+++ src/tick/index.ts
@@ -1,16 +1,24 @@
 import db from 'db';
 
 async function tick() {
-   await db.transaction(async tsx => {
-      await tsx('stars_players')
-         .update({
-            star_id: tsx.raw('??', 'next_star_id'),
-            next_star_id: null,
-         })
-         .whereNotNull('next_star_id');
-   });
+   try {
+      await db.transaction(async tsx => {
+         // Move players to their next star
+         await tsx('stars_players')
+            .update({
+               star_id: tsx.raw('??', 'next_star_id'),
+               next_star_id: null,
+            })
+            .whereNotNull('next_star_id');
 
-   db.destroy();
+         // Kick factions with < 0.1 influence from stars
+         await tsx('stars_factions')
+            .whereIn('id', q => q.select('id').from('stars_factions').where('influence', '<', '0.1'))
+            .del();
+      });
+   } finally {
+      db.destroy();
+   }
 }
 
 tick().catch(console.error);
